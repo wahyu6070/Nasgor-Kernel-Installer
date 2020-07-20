411033575cb34459473c9625f0fd4a6d6803a0cd
Wahyu6070 <47686437+Wahyu6070@users.noreply.github.com>
2020-07-20 22:48:21 +0700

    Add files via upload

--- install.sh
+++ install.sh
@@ -1,5 +1,8 @@
 # Nasgor Kernel Installer 1.0
 # Your script
+# BASE=folder home files
+#
+
 print " "
 print " Name kernel : $name"
 print " Date        : $date"
@@ -7,3 +10,6 @@ print " Device      : $device"
 print " Version     : $version"
 print " Developer   : $author"
 print " "
+
+chmod 777 $BASE/xz
+$BASE/xz -d $BASE/kernel.xz
